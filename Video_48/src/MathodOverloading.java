4588b49e4a8182d7c08473412218561a49744cc0
Pv007Pv <awd321321123@gmail.com>
2023-06-26 09:22:43 +0300

    add

---
+++ Video_48/src/MathodOverloading.java
@@ -0,0 +1,15 @@
+public class MathodOverloading {
+    public static void main( String[] args){
+        System.out.println(Dodaty(1,3));
+        System.out.println(Dodaty(1,3,4));
+    }
+    public static int Dodaty(int a, int b){   return a + b;    }
+    public static int Dodaty(int a, int b, int c) {    return a+b+c;     }
+    public static String Dodaty (String a, String b) {
+        return a + b;
+    }
+    public static int Dodaty (int[] iArray) {
+        int sum = 0; for (int i: iArray) { sum +=i;
+            return sum;
+    }
+}
